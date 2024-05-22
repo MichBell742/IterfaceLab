c6c14ef49a98ac4aa482177e065662b9885094b7
MichBell742 <michele.bellucci07@iisgubbio.edu.it>
2024-05-22 17:06:55 +0200

    aggiornato dati

--- helphour.py
+++ helphour.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit, QTableWidget, QTableWidgetItem
 from PyQt5 import uic
 import sys
  
@@ -7,19 +7,30 @@ import sys
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
-        uic.loadUi(r"C:\Users\giacomolillacci\Desktop\mainwindow.ui", self)
+        uic.loadUi(r"mainwindow.ui", self)
  
         # find the widgets in the xml file
         
-        #self.textedit = self.findChild(QLineEdit, "line1")
-        #self.button = self.findChild(QPushButton, "button")
-        #self.button.clicked.connect(self.clickedBtn)
- 
+        lista=[
+                {"nome":"Andrea","professore":"Marco","ore":"24"},
+                {"nome":"Marco","professore":"Andreoli","ore":"24"},
+                {"nome":"Ruspi","professore":"Luca","ore":"24"}
+            ]
+        
+        self.buttonRimuovi = self.findChild(QPushButton, "bRimuovi")
+        self.buttonRimuovi.clicked.connect(self.clickedBtn)
+        
+        self.tabella = self.findChild(QTableWidget, "tabellaAssociazione")
+        self.tabella.setColumnCount(3)
+        self.tabella.setRowCount(4)
+        self.tabella.setHorizontalHeaderLabels(["nome","cognome","ore"])
+        
+        self.tabella.setItem(0,0,QTableWidgetItem(lista[0]["nome"]))  
+        
         self.show()
- 
- 
- 
+        
     def clickedBtn(self):
+        
         atext = self.textedit.text()
         print(atext)
 
